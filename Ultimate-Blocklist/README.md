81927de3cfe60c03c7d50f9d57646676066640c2
Marc Runkel <marc@runkel.org>
2026-06-03 22:47:00 +0000

    initial commit

---
+++ Ultimate-Blocklist/README.md
@@ -0,0 +1,72 @@
+Ultimate-Blocklist
+==================
+
+A super blocklist made from the most popular ones on the web!
+
+### Note:
+When using the BitTorrent protocol using blocklists does *nothing*! If you are a part of the swarm of ip's
+yours can be logged, neither this nor any other IP block software can protect you from that. Read more [here](http://www.reddit.com/r/torrents/comments/17gold).
+
+## Docker Version
+
+```
+docker build -t ubl .
+docker run -it --name ubl ubl
+docker cp ubl:/usr/src/app/blocklist.txt .
+```
+
+or
+
+```
+make docker
+```
+
+## Python Version
+
+### Reqs:
+
+* `Python 2.7.8`
+
+---
+
+* Download latest release
+* (optional) Create a virtual environment ``virtualenv --no-site-packages env``
+* (optional) Activate the virtual environment ``source env/bin/activate``
+* Install the dependencies ``pip install -r requirements.txt``
+* Run `python UltimateBlockList.py`
+* Wait and your blocklist will be available in `blocklist.txt` when the script exits
+
+## Shell Verison
+
+ * **Generate a local brand-new list**
+     * Run using `./UpdateList.sh`
+     * `-c path_to_transmission_conf_dir` (if you use your system's default location, this option is unnecessary)
+     * `-z` flag will create gzipped file ( transmission daemon 2.84 won't load gzipped file properly )
+     * Note: You can find your config directory [here](https://trac.transmissionbt.com/wiki/ConfigFiles)
+     * Your blocklist will be loaded the next time you start Transmission
+ * **Setup on a webserver**
+     * Upload the folder content (or clone this repo) to your webserver
+     * Make sure the permission are right (755) for the scripts
+     * Add to your crontab
+     		Daily: 00 00 * * * ./UpdateList.sh
+     * Make your Torrent app point to your webserver like so: http://myserver.com/list.txt to download the newly generated list
+
+Special thanks to [alphapapa](https://github.com/alphapapa) for the idea behind the design of the new script.
+
+
+## Dropbox Version
+
+* **Build with [Docker](#docker-version)**
+
+* Setup a cron job to run the script using docker and upload to Dropbox
+using the environment variable `DROPBOX_ACCESS_TOKEN`.
+    * Ex. `0 5 * * * /usr/bin/docker run --rm -it -e DROPBOX_ACCESS_TOKEN="..." ubl`
+
+Thanks to [voxxit](//github.com/voxxit) for helping add this feature
+
+---
+
+## Contributors
+
+Please check [here](//github.com/walshie4/Ultimate-Blocklist/graphs/contributors) for a full list of contributors.
+
---
+++ Ultimate-Blocklist/UpdateList.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+# A simple script that downloads all blacklists in the list, and saves them in one mega-list
+# Written by: Adam Walsh
+# Written on 2/24/14
+
+#This has been tested on Ubuntu 12.04
+
+#-----CONFIG-----
+LIST="../public/blocklist.txt"     #This is the name of the final list file
+
+while getopts ":c:zh" opt; do
+  case $opt in
+    c)
+      CONF_DIR=$OPTARG
+      ;;
+    z)
+      zip=true
+      ;;
+    h)
+            echo -ne "Usage: -c config dir\n\t-z gzip result file ( doesn't work with daemon 2.84 )\n"
+            exit 0
+            ;;
+    \?)
+          echo "Invalid option: -$OPTARG" >&2
+          exit 1
+          ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+  esac
+done
+
+if [[ ! -z $CONF_DIR ]]; then
+  path_to_config=$CONF_DIR
+
+elif [[ $OSTYPE =~ "darwin" ]]; then
+  path_to_config=$HOME/Library/Application\ Support/Transmission
+
+else
+  path_to_config=$HOME/.config/transmission
+fi
+
+blocklist_path=$path_to_config/blocklists
+
+TITLEs=("Bluetack LVL 1" "Bluetack LVL 2" "Bluetack LVL 3" "Bluetack edu" "Bluetack ads"
+"Bluetack spyware" "Bluetack proxy" "Bluetack badpeers" "Bluetack Microsoft" "Bluetack spider"
+"Bluetack hijacked" "Bluetack dshield" "Bluetack forumspam" "Bluetack webexploit" "TBG Primary Threats"
+"TBG General Corporate Range" "TBG Buissness ISPs" "TBG Educational Institutions"
+)
+URLs=("http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_level2&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_level3&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_edu&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_ads&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_spyware&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_proxy&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_templist&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_microsoft&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_spider&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_hijacked&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=bt_dshield&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=ficutxiwawokxlcyoeye&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=ghlzqtqxnzctvvajwwag&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=ijfqtofzixtwayqovmxn&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=ecqbsykllnadihkdirsh&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=jcjfaxgyyshvdbceroxf&fileformat=p2p&archiveformat=gz"
+"http://list.iblocklist.com/?list=lljggjrpmefcwqknpalp&fileformat=p2p&archiveformat=gz"
+)
+#---END CONFIG---
+
+if tty -s; then
+  info() {
+    echo "$@"
+  }
+else # we're non-interactive, no output needed
+  info() {
+    true
+  }
+fi
+
+die() {
+  echo "$@"
+  exit 1
+}
+
+rm -f $LIST #delete the old list
+
+if wget=$(command -v wget); then
+  download() {
+    $wget -q --tries=2 --connect-timeout=2 --read-timeout=5 -O "list.gz" "$1"
+  }
+elif curl=$(command -v curl); then
+  download() {
+    $curl -m5 "$1" -L -o "list.gz"
+  }
+else
+  die "$0: 'wget' or 'curl' required but not found. Aborting."
+fi
+
+index=0
+for url in "${URLs[@]}"; do
+      title="${TITLEs[$index]}"
+      info "Downloading list $title"
+      if download "$url"
+      then
+        info "Adding IP's to list file..."
+        gunzip -c "list.gz" >> "$LIST"  || die "Cannot append to list" #append to list file
+        rm "list.gz" || die "Cannot remove downloaded file"
+        info ""
+      fi
+      index=$((index+=1))
+done
+
+#if [[ ! -z $zip ]]; then
+#  info "Zipping..."
+#  gzip -c $LIST > list.gz || die "Cannot gzip"
+#  info "Copying zipped list to $blocklist_path"
+#  cp list.gz "$blocklist_path/" || die "Cannot copy to $blocklist_path/"
+#else
+#  info "Copying list to $blocklist_path"
+#  cp list.txt "$blocklist_path/" || die "Cannot copy to $blocklist_path/"
+#fi
+
+wc -l $LIST || die "Cannot count lines" #print out some list stats
+rm -f list.*
+
+info "Done!"
+info "Restart transmission"
+
